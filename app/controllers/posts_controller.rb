c65a01ea7a0b31c8f1e926581ae18553e3e86601
ramakb <rama_kb@hotmail.com>
2015-01-27 11:24:05 +0000

    Loop through all posts for Index View

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:edit, :show, :update, :destroy]
   def index
+    @posts = Post.all.order("created_at DESC")
   end
   
    def show
-    
   end
   
   def new
